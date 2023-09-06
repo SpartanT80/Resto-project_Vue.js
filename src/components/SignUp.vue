a734f760afe19942dd60cd569eda74db0fcce69d
SpartanT80 <florian3194@hotmail.fr>
2023-09-06 22:39:29 +0200

    add DB, update SignUp

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,16 +2,40 @@
     <img class="logo" src="../assets/logo.svg" alt="logo restaurant">
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter your Name">
-        <input type="text" placeholder="Enter your Email">
-        <input type="password" placeholder="Enter your Password">
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter your Name">
+        <input type="text" v-model="email" placeholder="Enter your Email">
+        <input type="password" v-model="password" placeholder="Enter your Password">
+        <button v-on:click="signUp">Sign Up</button>
     </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
     name : "SignUp",
+    data(){
+        return {
+            name: "",
+            email: "",
+            password: "",
+        }
+    },
+    methods: {
+        async signUp()
+        {
+            let result = await axios.post("http://localhost:3000/users", {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            });
+
+            console.log(result);
+            if(result.status==201){
+                alert("signUp done !!");
+                localStorage.setItem("user-info", JSON.stringify(result.data))
+            }
+        }
+    }
 }
 </script>
 
