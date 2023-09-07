d2924aba93b4979f158b5b940dcc99608ed1205a
SpartanT80 <florian3194@hotmail.fr>
2023-09-07 17:07:37 +0200

    add logout logic, clear localStorage

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -3,13 +3,19 @@
         <a href="#">Home</a>
         <a href="#">Add Restaurant</a>
         <a href="#">Update Restaurant</a>
-        <a href="#">Logout</a>
+        <a v-on:click="logout" href="#">Logout</a>
     </nav>
 </template>
 
 <script>
 export default {
-    name: 'HeaderPage'
+    name: 'HeaderPage',
+    methods: {
+        logout(){
+            localStorage.clear();
+            this.$router.push({name:'LoginPage'})
+        }
+    }
 }
 </script>
 
