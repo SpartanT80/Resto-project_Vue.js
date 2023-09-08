08b2bdc20367c1d14755796b24a3694e0b58a356
SpartanT80 <florian3194@hotmail.fr>
2023-09-08 16:56:53 +0200

    finish update logic

--- src/components/UpdatePage.vue
+++ src/components/UpdatePage.vue
@@ -5,7 +5,7 @@
         <input type="text" name="name" placeholder="Enter name" v-model="restaurants.name">
         <input type="text" name="address" placeholder="Enter address" v-model="restaurants.address">
         <input type="text" name="contact" placeholder="Enter contact" v-model="restaurants.contact">
-        <button type="button" v-on:click="addRestaurant">Update Restaurant</button>
+        <button type="button" v-on:click="updateRestaurant">Update Restaurant</button>
     </form>
 </template>
 
@@ -26,13 +26,27 @@ export default {
             }
         }
     },
+    methods: {
+        async updateRestaurant() {
+            console.warn(this.restaurants);
+            // alert("Restaurant Updated");
+            const result = await axios.put('http://localhost:3000/restaurants/' + this.$route.params.id, {
+                name: this.restaurants.name,
+                address: this.restaurants.address,
+                contact: this.restaurants.contact
+            });
+            if (result.status == 200) {
+                this.$router.push({ name: 'HomePage' })
+            }
+        }
+    },
     async mounted() {
         let user = localStorage.getItem("user-info");
         if (!user) {
             this.$router.push({ name: 'SignUp' })
         }
 
-        const result = await axios.get('http://localhost:3000/restaurants/'+ this.$route.params.id);
+        const result = await axios.get('http://localhost:3000/restaurants/' + this.$route.params.id);
         this.restaurants = result.data;
     }
 }
